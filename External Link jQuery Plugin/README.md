79f3c0998fdfe0a530cf8d8d6a40f55cd4ab36dd
techmynd <admin@techmynd.com>
2018-07-08 04:51:21 +0500

    plugin files upload - first commit

---
+++ External Link jQuery Plugin/README.md
@@ -0,0 +1,52 @@
+# mjqExLinks
+
+mjqExLinks can be used to open external links in new window and apply optional attributes to external links such as rel (external, nofollow). Apply at whole web page or any section of webpage.
+
+- Dependency: jQuery
+- jQuery Support: jquery 1.12.4+
+- Web Browsers Support: All
+
+For **documentation**, usage, and examples, download and open index file in web browser.
+
+### How To Use
+
+Use files from dist folder. 
+
+Call mjqExLinks.min.js below jQuery script and initialize the plugin like below:
+
+Default initialization
+
+$(document).ready(function() {
+	$("body").mjqexlinks();
+});
+
+or
+$(".YourClass").mjqexlinks();
+or
+$("#anyID").mjqexlinks();
+
+Initialize with Options
+
+$(document).ready(function() {
+    $('body').mjqexlinks({
+        target: "_blank",		// Default is _blank 
+        className: "external",	// class name of your choice // comment line if you don't want to use it
+        nofollow: true,			// use rel="nofollow" // true or flase
+        external: false			// use rel="external" // true or flase
+    });
+});
+
+- If you want to apply this to whole webpage use body selector like below:
+$('body').mjqexlinks({ ... options ... });
+- If you do not want to use nofollow or external attributes, change true to false
+- target: '_blank', you can use any target, this plugin does not restrict you to use only target _blank
+
+### Contributing to this project
+
+We welcome all to participate in making this the best software it can be.
+
+### Credits
+
+- Concept and Development by [Muhammad Javed Khalil](https://javedkhalil.com)
+- Design and CSS by [Muhammad Javed Khalil](https://javedkhalil.com)
+- Repository maintained by [@techmynd](https://github.com/techmynd)
---
+++ External Link jQuery Plugin/assets/scripts/mjqExLinks.js
@@ -0,0 +1,88 @@
+/*
+jQuery Plugin
+Plugin Name: mjqExLinks 
+Author: Muhammad Javed Khalil
+Website: https://www.javedkhalil.com
+Description: Process external links of website and apply nofollow and more to only external links
+Benefits: Improves Website SEO
+*/
+
+(function ($) {
+
+    $.fn.mjqexlinks = function( options ) {
+ 
+        // Default Options
+        var settings = $.extend({
+            target: "_blank",	// use target="_blank" // will always be there if the plugin is activated // you can only change value
+            className: "",		// unique class, user input, default null
+            nofollow: true,	    // use rel="nofollow" // true or false
+            external: false	    // use rel="external" // true or false
+        }, options );
+ 
+        // Apply Options
+
+        
+        // get the links
+        // var getLinks = this.find('a');
+
+        // get only external links
+		var getLinks = this.find('a[href^="http://"]:not([href*="' + location.hostname + '"]), a[href^="https://"]:not([href*="' + location.hostname + '"])');
+
+		// get values from initialization from user file and store in our own variables
+		// get rel nofollow value
+		var getNoFollow = settings.nofollow;
+		// get rel external value
+		var getExternal = settings.external;
+		// get className value
+		var getClassName = settings.className;
+
+		// NOW
+		// getNoFollow, getExternal are true or false and getClassName has got class value if user has used it
+
+		// case 1
+		// if nofollow is true and external is false // only show rel="nofollow"
+		if(getNoFollow == true && getExternal == false) {
+			newRel = "nofollow";
+		}
+		// case 2
+		// if nofollow is false and external is true // only show rel="external"
+		else if(getExternal == true && getNoFollow == false) {
+			newRel = "external";
+		}
+		// case 3
+		// if nofollow is true and external is true // show rel="nofollow external"
+		else if(getExternal == true && getNoFollow == true) {
+			newRel = "nofollow external";
+		}
+
+		// just to print values to see if system is accepting the values from initialization
+		// settings.nofollow = "custom value check"; // set custom value
+		// console.log(settings.nofollow);
+
+		// if className is not empty, process the logic below 
+		if(getClassName != '') {
+	    
+			if(getExternal == false && getNoFollow == false) {
+		    	return getLinks.attr({ target: settings.target, class: getClassName });
+			}
+
+		    if(getExternal == true || getNoFollow == true) {
+		    	return getLinks.attr({ target: settings.target, rel: newRel, class: getClassName });
+			}
+
+		}
+		// if className is empty, process the logic below
+		else {
+			if(getExternal == false && getNoFollow == false) {
+		    	return getLinks.attr({ target: settings.target});
+			}
+
+		    if(getExternal == true || getNoFollow == true) {
+		    	return getLinks.attr({ target: settings.target, rel: newRel });
+			}			
+		}
+
+
+    };
+ 
+}( jQuery ));
